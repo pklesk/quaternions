e8c279b828f26063705c2e12a374c0be35bda247
Przemyslaw Klesk <pklesk@wi.zut.edu.pl>
2026-05-12 12:45:21 +0200

    experiments from 5090

--- src/main.py
+++ src/main.py
@@ -53,11 +53,11 @@ if __name__ == "__main__":
     t1_main = time.time()
          
     # experiment settings
-    M, N, P = 1000, 1000, 1000
+    M, N, P = 300, 300, 300
     SEED = 0    
     RANGE = 2.0
     DTYPE = np.float32 # {np.float32, np.float64}
-    REPETITIONS = 10
+    REPETITIONS = 100
     VERBOSE = False
     APPROACHES = {
         "QMATMUL_NAIVE_NUMBA_ST": (True, QMATMUL_NAIVE_NUMBA_ST_FUNCTIONS[DTYPE], {"verbose": False}),
