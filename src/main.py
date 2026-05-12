c1869772f695a49b9d2bb8ab67be3d130dc5183a
pklesk <pklesk@wi.zut.edu.pl>
2026-05-13 00:34:30 +0200

    xexperiment from 3090

--- src/main.py
+++ src/main.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     t1_main = time.time()
          
     # experiment settings
-    M, N, P = 1000, 3000, 2000
+    M, N, P = 3000, 3000, 3000
     SEED = 0    
     RANGE = 2.0
     DTYPE = np.float32 # {np.float32, np.float64}
