48233fbd04623080da7028b2f8b4272efbc118b2
pklesk <pklesk@wi.zut.edu.pl>
2026-05-14 00:51:41 +0200

    experiments on floats64 from 3090

--- src/main.py
+++ src/main.py
@@ -53,10 +53,10 @@ if __name__ == "__main__":
     t1_main = time.time()
          
     # experiment settings
-    M, N, P = 1000, 3000, 2000
+    M, N, P = 1000, 1000, 1000
     SEED = 0    
     RANGE = 2.0
-    DTYPE = np.float32 # {np.float32, np.float64}
+    DTYPE = np.float64 # {np.float32, np.float64}
     REPETITIONS = 10
     VERBOSE = False
     APPROACHES = {
